91fd44aaa8a86b1b5c7cd726816e975a3a642744
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-03-07 13:04:17 -0500

    Entity Baking

---
+++ Assets/Scripts/Dots/RotateSpeedAuthoring.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class RotateSpeedAuthoring : MonoBehaviour
+{
+    public float value;
+
+    private class Baker : Baker<RotateSpeedAuthoring>
+    {
+        public override void Bake(RotateSpeedAuthoring authoring)
+        {
+            Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+            
+            AddComponent(entity, new RotateSpeed()
+            {
+                value = authoring.value
+            });
+        }
+    }
+}
