f729be4fecfe639215fadaefd913b6b95e242bef
shototype <mattnewmanfrfr@gmail.com>
2025-03-09 22:31:30 -0400

    Added Menus, TMPro, temp score to ui

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject mainMenuParent;
+    public GameObject mainCreditsParent;
+    //public GameObject creditsMain;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadPlayGame()
+    {
+        SceneManager.LoadScene("EnemyTestScene");
+    }
+
+    public void LoadQuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenCreditsMain()
+    {
+        mainMenuParent.SetActive(false);
+        mainCreditsParent.SetActive(true);
+    }
+
+    public void CloseCreditsMain()
+    {
+        mainMenuParent.SetActive(true);
+        mainCreditsParent.SetActive(false);
+    }
+}
