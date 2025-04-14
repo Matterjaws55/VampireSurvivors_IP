5590d71e1b29b36acd90e6841d0ed6d2fdd4cb3b
shototype <mattnewmanfrfr@gmail.com>
2025-04-13 20:54:28 -0400

    Added try catch exception handler for loading levels
    Added a try catch exception handler to the gamemanager script. It controls loading levels/scenes, it should work without an exception for restarting the level on pause, but trying to load the main menu catches an exception.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,6 +12,10 @@ public class GameManager : MonoBehaviour
 
     public GameObject player;
 
+    public GameObject pauseParent;
+    private bool isPaused;
+    public GameObject healthCanvas;
+
     private void Start()
     {
         
@@ -18,7 +23,14 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            if (isPaused)
+                PauseGame();
+            else
+                UnPauseGame();
+        }
     }
 
     public void TakeDamage(float damage)
@@ -32,5 +44,77 @@ public class GameManager : MonoBehaviour
         healthAmount += healingAmount;
         healthAmount = Mathf.Clamp(healthAmount, 0, 100);
         healthBar.fillAmount = healthAmount / 100f;
+    }
+
+    public void PauseGame()
+    {
+        isPaused = true;
+        HideUI();
+        pauseParent.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void UnPauseGame()
+    {
+        isPaused = false;
+        pauseParent.SetActive(false);
+        UnHideUI();
+        Time.timeScale = 1;
+    }
+
+    public void HideUI()
+    {
+        
+        healthCanvas.SetActive(false);
+    }
+
+    public void UnHideUI()
+    {
+        
+        healthCanvas.SetActive(true);
+    }
+
+    public static bool LoadLvl(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+        Time.timeScale = 1.0f;
+        return true;
+    }
+
+    public void RestartScene()
+    {
+        try
+        {
+            LoadLvl(1);
+            Debug.Log("Level Restarted!");
+        }
+
+        catch (System.ArgumentException exception)
+        {
+            LoadLvl(0);
+            Debug.Log("Reverting to test scene" + exception.ToString());
+        }
+        
+    }
+
+    public void GoToMainMenu()
+    {
+        try
+        {
+            LoadLvl(-1);
+            Debug.Log("Level Restarted!");
+        }
+
+        catch (System.ArgumentException exception)
+        {
+            LoadLvl(1);
+            Debug.Log("Reverting to level scene" + exception.ToString());
+        }
+
+        finally
+
+        {
+            Debug.Log("Restarted Level as Main Menu does not yet exist on this branch");
+        }
     }
 }
