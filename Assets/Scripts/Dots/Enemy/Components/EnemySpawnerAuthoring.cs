984708db1ca2ccea1a1080d6fddcba7ef245d492
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-04-25 01:23:19 -0400

    Work on new enemy spawner

---
+++ Assets/Scripts/Dots/Enemy/Components/EnemySpawnerAuthoring.cs
@@ -0,0 +1,87 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Dots.Enemy.Components
+{
+        public class EnemySpawnerAuthoring : MonoBehaviour
+        {
+                [System.Serializable]
+                public struct EnemyTierAuthoring
+                {
+                        public GameObject prefab;
+                        public float timeToUnlock;
+                        public float spawnInterval;
+                        public float spawnRadius;
+                }
+
+                public EnemyTierAuthoring[] enemyTiers;
+                public Transform player;
+        }
+
+        public class EnemySpawnerBaker : Baker<EnemySpawnerAuthoring>
+        {
+                public override void Bake(EnemySpawnerAuthoring authoring)
+                {
+                        var entity = GetEntity(TransformUsageFlags.None);
+        
+                        // Bake enemy tiers
+                        var tiers = new NativeArray<EnemyTier>(authoring.enemyTiers.Length, Allocator.Temp);
+                        for (int i = 0; i < authoring.enemyTiers.Length; i++)
+                        {
+                                var tier = authoring.enemyTiers[i];
+                                // Use GetEntity with correct TransformUsageFlags for prefabs
+                                var prefabEntity = GetEntity(tier.prefab, TransformUsageFlags.Dynamic | TransformUsageFlags.Dynamic);
+            
+                                tiers[i] = new EnemyTier
+                                {
+                                        PrefabEntity = prefabEntity,
+                                        TimeToUnlock = tier.timeToUnlock,
+                                        SpawnInterval = tier.spawnInterval,
+                                        SpawnRadius = tier.spawnRadius
+                                };
+                        }
+
+                        // Add components
+                        AddComponent(entity, new EnemySpawnerComponent
+                        {
+                                PlayerEntity = GetEntity(authoring.player, TransformUsageFlags.Dynamic),
+                                GameTime = 0,
+                                NextSpawnTime = 0
+                        });
+
+                        var tiersList = new FixedList128Bytes<EnemyTier>();
+                        foreach (var tier in tiers)
+                        {
+                                tiersList.Add(tier);
+                        }
+
+                        AddComponent(entity, new EnemyTiersComponent
+                        {
+                                Tiers = tiersList
+                        });
+
+                        tiers.Dispose();
+                }
+        }
+        
+        public struct EnemyTier
+        {
+                public Entity PrefabEntity;
+                public float TimeToUnlock;
+                public float SpawnInterval;
+                public float SpawnRadius;
+        }
+
+        public struct EnemySpawnerComponent : IComponentData
+        {
+                public Entity PlayerEntity;
+                public float GameTime;
+                public float NextSpawnTime;
+        }
+
+        public struct EnemyTiersComponent : IComponentData
+        {
+                public FixedList128Bytes<EnemyTier> Tiers;
+        }
+}
---
+++ Assets/Scripts/Dots/Enemy/Systems/EnemySpawnerSystem.cs
@@ -0,0 +1,74 @@
+﻿using Dots.Enemy.Components;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Dots.Enemy.Systems
+{
+    public partial class EnemySpawnerSystem : SystemBase
+    {
+        private BeginSimulationEntityCommandBufferSystem _beginSimEcbSystem;
+
+        protected override void OnCreate()
+        {
+            _beginSimEcbSystem = World.GetOrCreateSystemManaged<BeginSimulationEntityCommandBufferSystem>();
+        }
+
+        protected override void OnUpdate()
+        {
+            var ecb = _beginSimEcbSystem.CreateCommandBuffer();
+            var deltaTime = SystemAPI.Time.DeltaTime;
+
+            foreach (var (spawner, tiers) in
+                     SystemAPI.Query<RefRW<EnemySpawnerComponent>, RefRO<EnemyTiersComponent>>())
+            {
+                spawner.ValueRW.GameTime += deltaTime;
+
+                if (spawner.ValueRO.GameTime < spawner.ValueRO.NextSpawnTime)
+                    continue;
+
+                var currentTier = GetCurrentTier(spawner.ValueRO.GameTime, tiers.ValueRO.Tiers);
+                if (!EntityManager.Exists(currentTier.PrefabEntity)) continue;
+                if (!EntityManager.Exists(spawner.ValueRO.PlayerEntity)) continue;
+
+                var playerTransform = SystemAPI.GetComponent<LocalTransform>(spawner.ValueRO.PlayerEntity);
+                int spawnCount = math.clamp((int)(spawner.ValueRO.GameTime / 60f) + 1, 1, 8);
+
+                for (int i = 0; i < spawnCount; i++)
+                {
+                    float angle = UnityEngine.Random.Range(0, math.PI * 2);
+                    float3 spawnPos = new float3(
+                        playerTransform.Position.x + math.cos(angle) * currentTier.SpawnRadius,
+                        playerTransform.Position.y,
+                        playerTransform.Position.z + math.sin(angle) * currentTier.SpawnRadius
+                    );
+
+                    var enemyEntity = ecb.Instantiate(currentTier.PrefabEntity);
+                    ecb.SetComponent(enemyEntity, LocalTransform.FromPositionRotationScale(
+                        spawnPos,
+                        quaternion.identity,
+                        1f
+                    ));
+                }
+
+                spawner.ValueRW.NextSpawnTime = spawner.ValueRO.GameTime + currentTier.SpawnInterval;
+            }
+
+            _beginSimEcbSystem.AddJobHandleForProducer(Dependency);
+        }
+
+        private static EnemyTier GetCurrentTier(float gameTime, FixedList128Bytes<EnemyTier> tiers)
+        {
+            for (int i = tiers.Length - 1; i >= 0; i--)
+            {
+                if (gameTime >= tiers[i].TimeToUnlock)
+                {
+                    return tiers[i];
+                }
+            }
+            return tiers[0];
+        }
+    }
+}
