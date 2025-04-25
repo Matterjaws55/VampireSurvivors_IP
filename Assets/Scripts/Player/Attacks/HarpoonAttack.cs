6bdd68ff3e2c88997c5887f3fd05469e257886e9
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-04-24 20:03:41 -0400

    Implement Harpoon

---
+++ Assets/Scripts/Player/Attacks/HarpoonAttack.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Player.Attacks
+{
+    using UnityEngine;
+
+    namespace Player.Attacks
+    {
+        public class HarpoonAttack : MonoBehaviour
+        {
+            private Vector3 _attackDirection;
+
+            [SerializeField] private float _speed = 20;
+            private void Start()
+            {
+                Vector3 screenCenter = new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0);
+                Vector3 mousePosition = Input.mousePosition;
+                _attackDirection = (mousePosition - screenCenter).normalized;
+
+                _attackDirection.z = _attackDirection.y;
+                _attackDirection.y = 0;
+                _attackDirection *= _speed;
+            }
+            
+            private void Update()
+            {
+                Vector3 pos = transform.localPosition;
+                pos += _attackDirection * Time.deltaTime;
+                transform.localPosition = pos;
+            }
+        }
+    }
+}
