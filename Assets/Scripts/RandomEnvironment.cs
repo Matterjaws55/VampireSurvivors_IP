ebb5fb7cd8f1eb290826d7e55afcd9df9844013a
shototype <mattnewmanfrfr@gmail.com>
2025-04-26 10:47:00 -0400

    added lilypads for random enviro

--- Assets/Scripts/RandomEnvironment.cs
+++ Assets/Scripts/RandomEnvironment.cs
@@ -7,13 +7,15 @@ public class RandomEnvironment : MonoBehaviour
 {
 
     public int numberOfBush;
+    public GameObject[] bushPrefabs;
 
     private void Start()
     {
-        Bushes();
+        //Bushes();
+        SpawnBushes();
     }
 
-    private void Bushes()
+    /*private void Bushes()
     {
         GameObject bushParent = new GameObject("Bush");
 
@@ -30,6 +32,31 @@ public class RandomEnvironment : MonoBehaviour
             var render = sphere.GetComponent<Renderer>();
             Color greenShade = new Color(0, Random.Range(0.2f, 1), 0);
             render.material.SetColor("_BaseColor", greenShade);
+        }
+    }
+*/
+
+    private void SpawnBushes()
+    {
+        GameObject bushParent = new GameObject("Bushes");
+
+        for (int i = 0; i < numberOfBush; i++)
+        {
+            
+            int randomIndex = Random.Range(0, bushPrefabs.Length);
+            GameObject selectedBush = bushPrefabs[randomIndex];
+
+            GameObject bushInstance = Instantiate(selectedBush);
+            bushInstance.transform.parent = bushParent.transform;
+
+            bushInstance.transform.position = new Vector3(Random.Range(-100, 100), 0.5f, Random.Range(-100, 100));
+
+            float randomYRotation = Random.Range(0f, 360f);
+            bushInstance.transform.rotation = Quaternion.Euler(0f, randomYRotation, 0f);
+
+            float xz = Random.Range(0.8f, 1.2f);
+            float y = Random.Range(0.8f, 1.5f);
+            bushInstance.transform.localScale = new Vector3(xz, y, xz);
         }
     }
 }
