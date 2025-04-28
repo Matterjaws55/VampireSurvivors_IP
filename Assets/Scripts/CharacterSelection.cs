3653b8550089d0fb17b8f6dd825442f17f5bc9b8
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-04-28 12:09:03 -0400

    Implement unlockable boats

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CharacterSelection : MonoBehaviour
 {
     public GameObject[] characters;
+    public int[] scoreCounts;
+
+    public Button _startButton;
+    public TMP_Text _scoreUnlockText;
     public int selectedCharacter = 0;
+
+    private int _highScore;
+
+    private void Start()
+    {
+        _highScore = PlayerPrefs.GetInt("HighScore", 0);
+        UpdateUnlockText();
+    }
 
     public void NextCharacter()
     {
         characters[selectedCharacter].SetActive(false);
         selectedCharacter = (selectedCharacter + 1) % characters.Length;
         characters[selectedCharacter].SetActive(true);
+        UpdateUnlockText();
     }
 
     public void PreviousCharacter()
@@ -24,6 +40,23 @@ public class CharacterSelection : MonoBehaviour
             selectedCharacter += characters.Length;
         }
         characters[selectedCharacter].SetActive(true);
+        UpdateUnlockText();
+    }
+
+    private void UpdateUnlockText()
+    {
+        if (_highScore > scoreCounts[selectedCharacter])
+        {
+            _scoreUnlockText.enabled = false;
+            _startButton.interactable = true;
+        }
+        else
+        {
+            _scoreUnlockText.enabled = true;
+            _scoreUnlockText.text = $"Unlock progress: {_highScore}/{scoreCounts[selectedCharacter]}";
+            _startButton.interactable = false;
+
+        }
     }
 
     public void StartGame()
