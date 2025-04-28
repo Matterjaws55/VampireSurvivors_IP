9dd6bfd1884919f9a06311bbd342d46f6bb06cc2
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-04-28 12:17:32 -0400

    Score Meter

--- Assets/Scripts/UI/ScoreDisplay.cs
+++ Assets/Scripts/UI/ScoreDisplay.cs
@@ -3,6 +3,7 @@ using Dots.Components;
 using TMPro;
 using Unity.Entities;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
@@ -17,6 +18,10 @@ namespace UI
         public delegate void OnUpgradeSignature();
         
         public static event OnUpgradeSignature OnUpgrade;
+
+        [SerializeField] private Image _scoreProgress;
+
+        private float _scoreStart = 0;
         
         private void Update()
         {
@@ -27,14 +32,33 @@ namespace UI
             if (scoreQuery.TryGetSingleton<Score>(out var score))
             {
                 _scoreText.text = $"Score: {score.Value}";
+                
+                if (_scoreProgress)
+                {
+                    
+                    float maxScore = 0;
+                    float curScore = score.Value;
+                    if(_currentUpgrade < _scoreThresholds.Length)
+                        maxScore = _scoreThresholds[_currentUpgrade];
+                    else
+                    {
+                        maxScore = _scoreStart +1;
+                        curScore = 0;
+                    }
+                    
+                    _scoreProgress.fillAmount = (curScore - _scoreStart) / (maxScore - _scoreStart);
+                }
             }
 
             if (_currentUpgrade < _scoreThresholds.Length && score.Value >= _scoreThresholds[_currentUpgrade])
             {
+                _scoreStart = _scoreThresholds[_currentUpgrade];
                 // Upgrade the player
                 OnUpgrade?.Invoke();
                 _currentUpgrade++;
             }
+
+
         }
 
 
