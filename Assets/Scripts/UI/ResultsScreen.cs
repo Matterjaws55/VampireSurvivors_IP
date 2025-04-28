01f038b288e2aeea405cc1b5fc2f6bd807114eaf
Joaqstarr <73140474+Joaqstarr@users.noreply.github.com>
2025-04-28 10:43:12 -0400

    Display results

---
+++ Assets/Scripts/UI/ResultsScreen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class ResultsScreen : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _scoreText;
+
+
+        private void Start()
+        {
+            int lastScore = PlayerPrefs.GetInt("LastScore", 0);
+            int highScore = PlayerPrefs.GetInt("HighScore", 0);
+            
+            if (lastScore > highScore)
+            {
+                PlayerPrefs.SetInt("HighScore", lastScore);
+                highScore = lastScore;
+            }
+            
+            _scoreText.text = $"SCORE: {lastScore}\nHIGH SCORE: {highScore}";
+        }
+    }
+}
